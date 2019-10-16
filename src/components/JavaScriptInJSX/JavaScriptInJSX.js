77210b36ade44652ce6224df4199f8e0c4289b00
Cat <blackcatstone@gmail.com>
2019-10-16 12:58:15 +0100

    Link to JSX intro

--- src/components/JavaScriptInJSX/JavaScriptInJSX.js
+++ src/components/JavaScriptInJSX/JavaScriptInJSX.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const JSinJSX = () => <p>You can do Javascript inline with JSX. To demonstrate, I will add 34627 and 964: {34627 + 964}</p>;
+const JSinJSX = () => <p>You can do Javascript inline with <a href="https://reactjs.org/docs/introducing-jsx.html">JSX</a>. To demonstrate, I will add 34627 and 964: {34627 + 964}</p>;
 
 export default JSinJSX;
