e03d28628249b8555c16b49f807b5595d912409c
Cat <blackcatstone@gmail.com>
2019-10-16 15:58:30 +0100

    Add routing to index

---
+++ src/Notfound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Notfound(props) {
+  return (
+    <div style={{margin:'0 auto',width:'72rem'}}>
+      <h1 style={{marginTop:200,fontSize:'6rem'}}>404 - OOPS!</h1>
+      <p>There isn't a page here.</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Notfound from './Notfound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Routing() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ App } />
+        <Route component={ Notfound } />
+      </Switch>
+    </Router>
+  );
+}
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
