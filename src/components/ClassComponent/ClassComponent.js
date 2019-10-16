5d1f1c9616726d88a6c1e496ba0389854cfdc925
Cat <blackcatstone@gmail.com>
2019-10-16 12:54:51 +0100

    Add more explanatory text to class component

--- src/components/ClassComponent/ClassComponent.js
+++ src/components/ClassComponent/ClassComponent.js
@@ -3,7 +3,10 @@ import React from 'react';
 class ClassComponent extends React.Component {
   render() {
     return (
-      <p>I am a class component!</p>
+      <>
+        <p>I am a class component!</p>
+        <p>Class components can store state, so are good for more complex components that need to change.</p>
+      </>
     );
   }
 }
